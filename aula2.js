00e0a5c1b069bfdcb4a0162dce34d37379dc9557
walter agostinho <walter.agostinho@outlook.com>
2024-04-24 23:01:18 -0300

    aula1 till aula3

---
+++ aula2.js
@@ -0,0 +1,4 @@
+"use strict"
+
+let nome="Walter";
+console.log(nome);
---
+++ aula3.js
@@ -0,0 +1,4 @@
+let nome= "Walter"; //dar preferência, variavel
+var nome2 = "Walter2"; // pode deixar inconsistência, variavel
+const nome3 = "Walter3" // constante que não pode ser mudada
+console.log(nome);

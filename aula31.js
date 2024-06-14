f302478f753045f6a3589468e7ee4e3dc61c1aec
walter agostinho <walter.agostinho@outlook.com>
2024-06-14 16:13:18 -0300

    aula32

--- aula31.js
+++ aula31.js
@@ -1,3 +1,7 @@
 const colecaoHTML=[...document.getElementsByName("div")]
 
 console.log(colecaoHTML)
+
+colecaoHTML.map((e)=>{
+    console.log(e)
+})
---
+++ aula32.js
@@ -0,0 +1,3 @@
+const cursosTodos=[...document.getElementsByClassName("curso")]
+
+console.log(cursosTodos)
